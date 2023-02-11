05be0b5fb830bed84a68564f53bb4822dfa3acd9
Ry2X <45420571+ry2x@users.noreply.github.com>
2023-02-12 03:59:14 +0900

    Update: Adopt to the Home

--- src/pages/tree/selectTree.tsx
+++ src/pages/tree/selectTree.tsx
@@ -1,32 +1,49 @@
-import HomeIcon from '@mui/icons-material/Home';
-import { Card } from '@mui/material';
+import { Home } from '@mui/icons-material';
+import { Button, Card, CardActions, CardContent, Container, Grid, Typography } from '@mui/material';
 import Link from 'next/link';
+
 import { useState } from 'react';
 import SelectTree from '@/components/treeSelect';
 import { data } from '@/constants/data';
+import style from '@/styles/Home.module.css';
 
 const Index = () => {
   const initId = '1';
   const [selectedId, setSelectedId] = useState<string>(initId);
   return (
-    <>
-      <br />
-      <Link href='/'>
-        <HomeIcon style={{ verticalAlign: 'bottom' }} />
-        To Home
-      </Link>
-      <br />
-      <Card style={{ textAlign: 'center', width: '30vw', height: '50vh' }}>
-        <br />
-        <title>SelectTree</title>
-        <Card style={{ padding: '-1%' }}>
-          <h1>Select Tree</h1>
-        </Card>
-        <br />
-        <h3>selected ID is :{selectedId}</h3>
-        <SelectTree data={data} initId={initId} onChange={(id) => setSelectedId(id)} />
-      </Card>
-    </>
+    <Container>
+      <Grid container>
+        <Grid xs={12} item>
+          <Card className={style.title}>
+            <h2>
+              <Link href='/' style={{ color: 'black' }}>
+                <Home className={style.inLineIcons} />
+                Home
+              </Link>
+            </h2>
+          </Card>
+        </Grid>
+        <Grid xs={12} item>
+          <br />
+        </Grid>
+        <Grid xs={6} item>
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color='text.secondary' gutterBottom>
+                SelectTrees
+              </Typography>
+              <Typography variant='h5' component='div'>
+                Trees In The SelectBox
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color='text.secondary'>
+                Slected Id is :{selectedId}
+              </Typography>
+              <SelectTree data={data} initId={initId} onChange={(id) => setSelectedId(id)} />
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
